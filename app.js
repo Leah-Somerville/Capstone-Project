1783dfe3b67957b387ed8bc6e5f9b60cd8dd3baf
Leah Somerville <leah.somerville4@gmail.com>
2023-05-01 11:17:14 -0500

    Built main buttons

---
+++ app.js
@@ -0,0 +1,29 @@
+// Getting HTML elements
+// Subject Buttons
+const currentEvents = document.querySelector(`#current-events`);
+const skillsInfo = document.querySelector(`#skills-info`);
+const math = document.querySelector(`#math`);
+// Flashcard
+const flashcard = document.querySelector(`#flashcard`);
+const answer = document.querySelector(`input`);
+const buzzer = document.querySelector(`#submit`);
+
+// Quest Objects
+const currentEventQuestions = {
+    {`Question 1`},
+};
+
+// Functions
+
+
+
+// When subject button is clicked
+currentEvents.addEventListener(`onclick`, () => {
+    
+})
+skillsInfo.addEventListener(`onclick`, () => {
+
+})
+math.addEventListener(`onclick`, () => {
+
+})
